30a38876a8b0920fd7331adf2c2f526270c8f7f6
lipeiv <lipeiv@qq.com>
2019-05-19 11:05:54 +0800

    Swift

---
+++ 我的第一个swift程序.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print("Hello World!\n\n我是李培培\n")
+let maxLoginAttempts = 10
+var currentLogininAttempt = 3
+currentLogininAttempt = 3
+var x = 1,y = 2,z = 3
+let PI = 3.14159 ;
+let SPEED0FLIGHT = 300_000;
+print("圆周率的值是\(PI)")
+print(str)
+
+let foo = 1
+var a = 1
+var b = 2
+var str2 = "a + b = \(a + b)"
+print(str2)
+
+/*
+var opt:Int? = nil
+opt = 1
+if (opt != nil) {
+    print(opt!)}
+*/
+
+var arr = [Int]()
+arr.append(1)
+print(arr[0])
